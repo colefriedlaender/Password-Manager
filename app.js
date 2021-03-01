c53c932bc370be64dfc73d013b9c161fab056939
colefriedlaender <colefriedlaender@outlook.de>
2021-03-01 18:12:32 +0100

    setAndGetPassword

--- app.js
+++
@@ -1,8 +0,0 @@
-console.log("Hallo!!");
-var command = process.argv.slice(2)[0];
-// const [, , command] = process.argv;
-if (command === "get") {
-  console.log("You like to get somthing");
-} else if (command === "set") {
-  console.log("You like to set somthing");
-}
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,58 @@
-const [command] = process.argv.slice(2);
-// const [, , command] = process.argv;
-if (command === "get") {
-  console.log("You like to get somthing");
-} else if (command === "set") {
-  console.log("You like to set somthing");
-}
+import prompts from "prompts";
+
+const name = async () => {
+  const response = await prompts({
+    type: "text",
+    name: "value",
+    message: "Who are you? ",
+  });
+  if (response.value === "Cole") {
+    password();
+  } else {
+    console.log(`Ups, somthing went wrong!`);
+    console.log(`Try again`);
+    name();
+  }
+};
+name();
+
+const password = async () => {
+  const response = await prompts({
+    type: "password",
+    name: "value",
+    message: "What is your Password?",
+  });
+  if (response.value === "1234") {
+    action();
+  } else {
+    console.log("That is not your Password!");
+    console.log(`Try again`);
+    password();
+  }
+};
+
+const action = async () => {
+  const response = await prompts({
+    type: "text",
+    name: "value",
+    message: "Do you want set or get a password",
+  });
+  if (response.value === "set") {
+    console.log("Now you can set your Password");
+    setpassword();
+  } else if (response.value === "get") {
+    console.log("Your Password is 1234");
+  } else {
+    console.log("Enter set or get");
+    action();
+  }
+};
+
+const setpassword = async () => {
+  const response = await prompts({
+    type: "password",
+    name: "value",
+    message: "Set a Password",
+  });
+  console.log("Congratulation you have set a new Password");
+};
